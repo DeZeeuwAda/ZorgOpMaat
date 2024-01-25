679f8ae63c1c1fb898480787ca943315f6557fb1
thomastemmink1999 <t.temmink@ad-academie.nl>
2024-01-25 17:04:39 +0100

    Create AfspraakOverzicht.java

---
+++ src/main/java/com/example/zorgopmaat/AfspraakOverzicht.java
@@ -0,0 +1,20 @@
+package com.example.zorgopmaat;
+
+// In de AfspraakOverzicht-klasse
+public class AfspraakOverzicht {
+    private String patient;
+    private String tijd;
+    private String locatie;
+    private String zorgverlener;
+
+    // Constructor met argumenten
+    public AfspraakOverzicht(String patient, String tijd, String locatie, String zorgverlener) {
+        this.patient = patient;
+        this.tijd = tijd;
+        this.locatie = locatie;
+        this.zorgverlener = zorgverlener;
+    }
+
+    // Andere methoden en eigenschappen
+}
+
